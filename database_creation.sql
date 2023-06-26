86099168c0191189dca3e814b586e564498aa6d3
Dorian Tarin <dorian.tarin@isen-ouest.yncrea.fr>
2023-06-26 15:51:48 +0200

    rework db structure

--- database_creation.sql
+++ database_creation.sql
@@ -103,26 +103,6 @@ CREATE TABLE gravite(
 
 
 #------------------------------------------------------------
-# Table: accident
-#------------------------------------------------------------
-
-CREATE TABLE accident(
-        id_accident  Int  Auto_increment  NOT NULL ,
-        age          Numeric NOT NULL ,
-        date         Date NOT NULL ,
-        heure        Numeric NOT NULL ,
-        ville        Varchar (50) NOT NULL ,
-        longi        Float NOT NULL ,
-        lati         Float NOT NULL ,
-        added_status Bool NOT NULL ,
-        id_gravite   Numeric
-	,CONSTRAINT accident_PK PRIMARY KEY (id_accident)
-
-	,CONSTRAINT accident_gravite_FK FOREIGN KEY (id_gravite) REFERENCES gravite(id_gravite)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
 # Table: ville
 #------------------------------------------------------------
 
@@ -134,31 +114,39 @@ CREATE TABLE ville(
 
 
 #------------------------------------------------------------
-# Table: comprend
+# Table: accident
 #------------------------------------------------------------
 
-CREATE TABLE comprend(
-        id_descr_athmo        Numeric NOT NULL ,
-        id_descr_cat_veh      Numeric NOT NULL ,
-        id_accident           Int NOT NULL ,
-        id_descr_intersection Numeric NOT NULL ,
-        id_descr_agglo        Numeric NOT NULL ,
-        id_descr_type_col     Numeric NOT NULL ,
-        id_descr_secu         Numeric NOT NULL ,
+CREATE TABLE accident(
+        id_accident           Int  Auto_increment  NOT NULL ,
+        age                   Numeric NOT NULL ,
+        date                  Date NOT NULL ,
+        heure                 Numeric NOT NULL ,
+        ville                 Varchar (50) NOT NULL ,
+        longi                 Float NOT NULL ,
+        lati                  Float NOT NULL ,
+        added_status          Bool NOT NULL ,
+        id_gravite            Numeric ,
         id_descr_lum          Numeric NOT NULL ,
         id_descr_surface      Numeric NOT NULL ,
-        code_insee            Numeric NOT NULL
-	,CONSTRAINT comprend_PK PRIMARY KEY (id_descr_athmo,id_descr_cat_veh,id_accident,id_descr_intersection,id_descr_agglo,id_descr_type_col,id_descr_secu,id_descr_lum,id_descr_surface,code_insee)
+        id_descr_secu         Numeric NOT NULL ,
+        id_descr_type_col     Numeric NOT NULL ,
+        id_descr_intersection Numeric NOT NULL ,
+        id_descr_agglo        Numeric NOT NULL ,
+        id_descr_cat_veh      Numeric NOT NULL ,
+        code_insee            Numeric NOT NULL ,
+        id_descr_athmo        Numeric NOT NULL
+	,CONSTRAINT accident_PK PRIMARY KEY (id_accident)
 
-	,CONSTRAINT comprend_descr_athmo_FK FOREIGN KEY (id_descr_athmo) REFERENCES descr_athmo(id_descr_athmo)
-	,CONSTRAINT comprend_descr_cat_veh0_FK FOREIGN KEY (id_descr_cat_veh) REFERENCES descr_cat_veh(id_descr_cat_veh)
-	,CONSTRAINT comprend_accident1_FK FOREIGN KEY (id_accident) REFERENCES accident(id_accident)
-	,CONSTRAINT comprend_descr_intersection2_FK FOREIGN KEY (id_descr_intersection) REFERENCES descr_intersection(id_descr_intersection)
-	,CONSTRAINT comprend_descr_agglo3_FK FOREIGN KEY (id_descr_agglo) REFERENCES descr_agglo(id_descr_agglo)
-	,CONSTRAINT comprend_descr_type_col4_FK FOREIGN KEY (id_descr_type_col) REFERENCES descr_type_col(id_descr_type_col)
-	,CONSTRAINT comprend_descr_secu5_FK FOREIGN KEY (id_descr_secu) REFERENCES descr_secu(id_descr_secu)
-	,CONSTRAINT comprend_descr_lum6_FK FOREIGN KEY (id_descr_lum) REFERENCES descr_lum(id_descr_lum)
-	,CONSTRAINT comprend_descr_surface7_FK FOREIGN KEY (id_descr_surface) REFERENCES descr_surface(id_descr_surface)
-	,CONSTRAINT comprend_ville8_FK FOREIGN KEY (code_insee) REFERENCES ville(code_insee)
+	,CONSTRAINT accident_gravite_FK FOREIGN KEY (id_gravite) REFERENCES gravite(id_gravite)
+	,CONSTRAINT accident_descr_lum0_FK FOREIGN KEY (id_descr_lum) REFERENCES descr_lum(id_descr_lum)
+	,CONSTRAINT accident_descr_surface1_FK FOREIGN KEY (id_descr_surface) REFERENCES descr_surface(id_descr_surface)
+	,CONSTRAINT accident_descr_secu2_FK FOREIGN KEY (id_descr_secu) REFERENCES descr_secu(id_descr_secu)
+	,CONSTRAINT accident_descr_type_col3_FK FOREIGN KEY (id_descr_type_col) REFERENCES descr_type_col(id_descr_type_col)
+	,CONSTRAINT accident_descr_intersection4_FK FOREIGN KEY (id_descr_intersection) REFERENCES descr_intersection(id_descr_intersection)
+	,CONSTRAINT accident_descr_agglo5_FK FOREIGN KEY (id_descr_agglo) REFERENCES descr_agglo(id_descr_agglo)
+	,CONSTRAINT accident_descr_cat_veh6_FK FOREIGN KEY (id_descr_cat_veh) REFERENCES descr_cat_veh(id_descr_cat_veh)
+	,CONSTRAINT accident_ville7_FK FOREIGN KEY (code_insee) REFERENCES ville(code_insee)
+	,CONSTRAINT accident_descr_athmo8_FK FOREIGN KEY (id_descr_athmo) REFERENCES descr_athmo(id_descr_athmo)
 )ENGINE=InnoDB;
 
