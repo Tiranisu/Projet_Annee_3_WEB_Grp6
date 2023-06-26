bf5aaa8d1d32ec7f7634846270c1553b7127619b
Mael G_N <mael.grelneau@gmail.com>
2023-06-26 08:10:31 +0000

    init server

--- php/constants.php
+++ php/constants.php
@@ -1,7 +1,13 @@
 <?php
-    const DB_USER = 'projetweb';
-    const DB_PASSWORD = 'groupe4';
-    const DB_NAME = 'sportisen';
-    const DB_SERVER = '127.0.0.1';
-    const DB_PORT = 5432;
+//Author: Prenom NOM
+//Login : etuXXX
+//Groupe: ISEN X GROUPE Y
+//Annee:
+
+
+
+
+
+
+
 ?>
