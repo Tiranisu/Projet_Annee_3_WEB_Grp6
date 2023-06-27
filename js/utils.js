84c142cec8b3ec956c1dfc29e4a6deb39a1f3b95
Dorian Tarin <dorian.tarin@isen-ouest.yncrea.fr>
2023-06-27 15:05:13 +0200

    basics F5

--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,60 @@
-/*
-Author: Prenom NOM
-Login : etuXXX
-Groupe: ISEN X GROUPE Y
-Annee:
-*/
 
+'use strict'
 
+//------------------------------------------------------------------------------
+//--- ajaxRequest --------------------------------------------------------------
+//------------------------------------------------------------------------------
+// Perform an Ajaxs request.
+// \param type The type of the request (GET, DELETE, POST, PUT).
+// \param url The url of the request.
+// \param callback The callback to call when the request success.
+// \param data The data associated with the request.
+function ajaxRequest(type, url, callback, data = null)
+{
+  let xhr;
+
+  // Create XML HTTP request.
+  xhr = new XMLHttpRequest();
+  if (type == 'GET' && data != null)
+    url += '?' + data;
+  xhr.open(type, url);
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+  // Add response handler.
+  xhr.onload = () =>
+  {
+    switch (xhr.status)
+    {
+      case 200:
+      case 201:
+        console.log(xhr.responseText);
+        callback(JSON.parse(xhr.responseText));
+        break;
+      default:
+        displayErrors(xhr.status);
+    }
+  };
+
+  // Send XML HTTP request.
+  xhr.send(data);
+}
+
+//------------------------------------------------------------------------------
+//--- displayErrors ------------------------------------------------------------
+//------------------------------------------------------------------------------
+// Display an error message accordingly to an error code.
+// \param errorCode The error code (HTTP status for example).
+function displayErrors(errorCode)
+{
+  let messages = {
+    400: 'Requête incorrecte',
+    401: 'Authentifiez vous',
+    404: 'Page non trouvée',
+    500: 'Erreur interne du serveur',
+    503: 'Service indisponible'
+  };
+
+  // Display error.
+  if (messages[errorCode] != undefined)
+    alert('Erreur : ' + messages[errorCode]);
+}
--- php/constants.php
+++ php/constants.php
@@ -1,8 +1,9 @@
 <?php
-//Author: Prenom NOM
-//Login : etuXXX
-//Groupe: ISEN X GROUPE Y
-//Annee:
+define('DB_SERVER','127.0.0.1');
+define('DB_PORT','3306');
+define('DB_NAME','etu750');
+define('DB_USER','etu750');
+define('DB_PASSWORD','blziyxos');
 
 
 
