b8af6662e09ec926af9d6607fc500c020c07551d
twist35 <etu746@vps-d2147abe.vps.ovh.net>
2023-06-27 06:51:02 +0000

    bdd ok remplie

--- donnees.py
+++ donnees.py
@@ -46,9 +46,8 @@ mydatabase = mysql.connector.connect(
 
 mycursor = mydatabase.cursor()
 for index, ligne in data.iterrows():
-    sql = "INSERT INTO accident(age, date, heure,ville, longi,lati,added_status,id_gravite,id_descr_lum,id_descr_surface,id_descr_secu,id_descr_type_col,id_descr_intersection,id_descr_agglo,id_descr_cat_veh,code_insee,id_descr_athmo) VALUES (%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, %s)"
-    val = ligne['age'], ligne['date'], ligne['heure'],ligne['ville'],ligne['longitude'],ligne['latitude'],,ligne['desc_grv'],ligne['descr_lum'],ligne['descr_etat_surf'],ligne['descr_dispo_secu'],ligne['descr_type_col'],ligne['description_intersection'],ligne['descr_agglo'],ligne['descr_cat_veh'],ligne['id_code_insee'],ligne['descr_athmo']
+    sql = "INSERT INTO accident(age, date, heure,ville, longi,lati,added_status,id_gravite,id_descr_lum,id_descr_surface,id_descr_secu,id_descr_type_col,id_descr_intersection,id_descr_agglo,id_descr_cat_veh,code_insee,id_descr_athmo) VALUES (%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s)"
+    val = ligne['age'], ligne['date'], ligne['heure'],ligne['ville'],ligne['longitude'],ligne['latitude'],0,ligne['descr_grav'],ligne['descr_lum'],ligne['descr_etat_surf'],ligne['descr_dispo_secu'],ligne['descr_type_col'],ligne['description_intersection'],ligne['descr_agglo'],ligne['descr_cat_veh'],ligne['id_code_insee'],ligne['descr_athmo']
     mycursor.execute(sql, val)
-    #print(row['ville'])
 mydatabase.commit()
 
