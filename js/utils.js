a9adb7fd5495829f847c8ac6831249176f6216ef
twist35 <etu746@vps-d2147abe.vps.ovh.net>
2023-06-27 14:30:32 +0000

    json pour carte

--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,71 @@
-/*
-Author: Prenom NOM
-Login : etuXXX
-Groupe: ISEN X GROUPE Y
-Annee:
-*/
+/**
+ * @Author: Thibault Napoléon <Imothep>
+ * @Company: ISEN Yncréa Ouest
+ * @Email: thibault.napoleon@isen-ouest.yncrea.fr
+ * @Created Date: 23-Jan-2018 - 17:00:53
+ * @Last Modified: 08-Dec-2019 - 19:12:57
+ */
 
+'use strict';
+
+//------------------------------------------------------------------------------
+//--- ajaxRequest --------------------------------------------------------------
+//------------------------------------------------------------------------------
+// Perform an Ajax request.
+// \param type The type of the request (GET, DELETE, POST, PUT).
+// \param url The url with the data.
+// \param callback The callback to call where the request is successful.
+// \param data The data associated with the request.
+function ajaxRequest(type, url, callback, data = null)
+{
+  let xhr;
+
+  // Create XML HTTP request.
+  xhr = new XMLHttpRequest();
+  if (type == 'GET' && data != null)
+    url += '?' + data;
+  xhr.open(type, url);
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+  // Add the onload function.
+  xhr.onload = () =>
+  {
+    switch (xhr.status)
+    {
+      case 200:
+      case 201:
+        //console.log(xhr.responseText);
+        callback(JSON.parse(xhr.responseText));
+        break;
+      default:
+        httpErrors(xhr.status);
+    }
+  };
+
+  // Send XML HTTP request.
+  xhr.send(data);
+}
+
+//------------------------------------------------------------------------------
+//--- httpErrors ---------------------------------------------------------------
+//------------------------------------------------------------------------------
+// Display an error message accordingly to an error code.
+// \param errorCode The error code (HTTP status for example).
+function httpErrors(errorCode)
+{
+  let messages = {
+    400: 'Requête incorrecte',
+    401: 'Authentifiez vous',
+    403: 'Accès refusé',
+    404: 'Page non trouvée',
+    500: 'Erreur interne du serveur',
+    503: 'Service indisponible'
+  };
+
+  // Display error.
+  if (errorCode in messages)
+  {
+    console.log(messages[errorCode])
+  }
+}
 
