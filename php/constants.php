10e5ffcc58f234a7dbfda88ee83a7d88bde57644
Dorian Tarin <dorian.tarin@isen-ouest.yncrea.fr>
2023-06-28 09:55:44 +0200

    fix bug

---
+++ php/constants.php
@@ -0,0 +1,8 @@
+<?php
+  // Database constants.
+  define('DB_SERVER', '127.0.0.1');
+  define('DB_PORT', '3306');
+  define('DB_NAME', 'etu750');
+  define('DB_USER', 'etu750');
+  define('DB_PASSWORD', 'blziyxos');
+?>
