bec0982f2c3677d728b965d6260d2a063ba8af00
Poulpe7 <teorortais@gmail.com>
2023-06-28 14:18:30 +0000

    Suite du commit precedent

--- js/kmeans.js
+++ js/kmeans.js
@@ -1,47 +1,45 @@
 function test(data)
 {
+    // Pour le message de chargement
     var loadingDiv = document.getElementById('loading');
+    var mainDiv = document.getElementById('div_style_map_clusters');
+    mainDiv.style.display = 'none';
     loadingDiv.style.display = 'flex';
 
-    //console.log(data);
-	//console.log(data[0]);
-    //console.log(data[1]['lati']);
+    // Préparation des données
 	jdata = data.data;
-	//console.log(jdata.map(row => row[0]));
-
     let tab_lat = jdata.map(row => row[0]);
     let tab_lon = jdata.map(row => row[1]);
     let tab_cluster = jdata.map(row => row[2]);
 
-    //console.log(tab_lon)
-
-    
-    // Données géographiques
+    // Données pour la carte
     var data = [{
         type: 'scattermapbox',
-        lat: tab_lat, // Tableau des latitudes
-        lon: tab_lon, // Tableau des longitudes
+        lat: tab_lat, 
+        lon: tab_lon,
         mode: 'markers',
         marker: {
             size: 8,
             color: tab_cluster
         }
     }];
+
     // Configuration de la carte
     var layout = {
         mapbox: {
             style: 'open-street-map',
-            center: { lat: 47, lon: 2.5 }, // Coordonnées de centrage de la carte
-            zoom: 4 // Niveau de zoom initial
+            center: { lat: 47, lon: 2.5 }, 
+            zoom: 4 
         },
         margin: { l: 0, r: 0, t: 0, b: 0 },
         showlegend: false
     };
+
     // Création de la carte
     Plotly.newPlot('div_style_map_clusters', data, layout);
 
+    // On affiche la div de la carte
     loadingDiv.style.display = 'none';
-    var mainDiv = document.getElementById('div_style_map_visu');
     mainDiv.style.display = 'flex';
 }
 
